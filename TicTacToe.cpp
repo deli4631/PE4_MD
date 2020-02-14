ed58614e91d16ae16b857a0e47ee18d6545d3c91
Dennis Lim <deli4631@colorado.edu>
2020-02-13 17:55:47 -0700

    fix formating issues

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -33,9 +33,9 @@ class TicTacToe {
     private: 
 	    Title board_[3][3];
 
-        /**
-         * creates a 3 by 3 arr of type Title
-        */
+    /**
+     * creates a 3 by 3 arr of type Title
+    */
 	void CreateBoard() {
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
