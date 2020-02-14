416a92c844bbe09cad57261160e021251a4c04a0
Dennis Lim <deli4631@colorado.edu>
2020-02-13 18:38:50 -0700

    adding the place marker function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,6 +11,7 @@ using namespace std;
 // enum class TilesSquare = {X_title, O_title};
 enum class Title {X, O, blank};
 
+
 std::string to_emoji(Title title){
     if(title == Title::X){
         return "❌";
@@ -38,6 +39,18 @@ class TicTacToe {
         */
         TicTacToe() {
             CreateBoard(); 
+            int turn = 0;
+        }
+
+        /**
+         * creates a 3 by 3 arr of type Title
+        */
+        void CreateBoard() {
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    board_[i][j] = Title::blank; 
+                }
+            }
         }
 
         // will display the board 
@@ -50,18 +63,26 @@ class TicTacToe {
             }
         }
 
+        // place marker will get the position of the player and mark it with an x or o
+        bool PlaceMarker(Position pos){
+            Title mark;
+            if(turn == 0){  
+                mark = Title::X
+            } else{
+                mark = Title::O
+            }
+
+            if(board_[pos.row][pos.col] != Title::blank){
+                board[pos.row][pos.col] = mark;
+                return true;
+            } 
+            return false;
+        }
+
     private: 
 	    Title board_[3][3];
-        /**
-         * creates a 3 by 3 arr of type Title
-        */
-        void CreateBoard() {
-            for(int i = 0; i < 3; i++) {
-                for(int j = 0; j < 3; j++) {
-                    board_[i][j] = Title::blank; 
-                }
-            }
-        }
+        std::vector<std::string> players;
+        int turn;
 };
 
 /**
@@ -71,3 +92,6 @@ int main() {
 	// Calls CreateBoard() with the constructor function
 	TicTacToe game; 
 }
+
+
+return Position{choice1, choice}
